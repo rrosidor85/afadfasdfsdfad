fa3d8449ab89137c6742155c8f6acb453475cbab
rrosidor85 <129698976+rrosidor85@users.noreply.github.com>
2023-04-04 18:45:54 -0400

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,45 +1,23 @@
 import React from 'react';
 import { View, Text, Image, ImageBackground, ScrollView } from 'react-native';
-import Carousel from 'react-native-snap-carousel'; // Import Carousel
+import Carousel from 'react-native-snap-carousel';
+import LinearGradient from 'react-native-linear-gradient'; // Import LinearGradient
 import Search from './Search';
 import Testimonials from './Testimonials';
 import styles from './styles';
 
-const image1 = require('../assets/image1.jpeg');
-const image2 = require('../assets/image2.jpeg');
-const image3 = require('../assets/image3.jpeg');
-
 const Home = () => {
-  const carouselItems = [
-    { image: image1 },
-    { image: image2 },
-    { image: image3 },
-  ];
-
-  const renderItem = ({ item }) => {
-    return (
-      <ImageBackground source={item.image} style={styles.carouselContainer}>
-        {/* Add any overlay content you want on top of the carousel images */}
-      </ImageBackground>
-    );
-  };
+  // Your component code here
 
   return (
-    <ScrollView style={styles.homeContainer}>
-      <Carousel
-        data={carouselItems}
-        renderItem={renderItem}
-        sliderWidth={styles.carouselContainer.width}
-        itemWidth={styles.carouselContainer.width}
-      />
-      <View style={styles.contentContainer}>
-        <View style={styles.searchContainer}>
-          <Text style={styles.rentARec}>Rent-A-Rec</Text>
-          <Search />
-        </View>
-        <Testimonials />
-      </View>
-    </ScrollView>
+    <LinearGradient
+      colors={['#1E88E5', '#FFC107']}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.homeContainer}
+    >
+      {/* Your component contents here */}
+    </LinearGradient>
   );
 };
 
